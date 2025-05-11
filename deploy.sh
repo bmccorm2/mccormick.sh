b5f10ebf0176c2b9559e330bb3605717c00c3799
Bryan McCormick <bryan@mccormick.sh>
2025-05-10 19:25:29 -0600

    updated nextcloud config

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-rsync -trhPuL --exclude-from=deployExclude.txt --delete docker mccormick.sh:/opt
+rsync -rhPuL \
+  --exclude-from=deployExclude.txt \
+  --exclude='env/' \
+  --filter='protect env/' \
+  --delete \
+  docker \
+  mccormick.sh:/opt   
 
-# Service must be built on host machine because arch is different
-# rsync -trhPuLv --exclude-from=deployExclude.txt --exclude service/node_modules --delete service 1mccormick:/opt/temp
+rsync -rhPuL \
+  config/ \
+  mccormick.sh:/opt/config
 
 echo Complete!
