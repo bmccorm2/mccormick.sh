36de11127ec2b2731ccfb378032b1af083634c4b
Bryan McCormick <bryan@mccormick.sh>
2026-02-12 23:34:55 -0700

    added rspamd and removed opendkim

--- docker/opendkim/createKeys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# just change the directory if this command doesn't work
-# run this for each domain and copy the results into the correct folder!
-opendkim-genkey -b 2048 -d firestonelodging.com -D /tmp/fir -s default -v
-
-# opendkim-testkey -d mccormick.sh -s default -vvv
--- docker/opendkim/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p /var/spool/postfix/opendkim
-chown -R opendkim:opendkimSock /var/spool/postfix/opendkim
-
-exec opendkim -f -l -x /etc/opendkim/opendkim.conf
