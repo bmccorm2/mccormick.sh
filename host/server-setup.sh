77dee15a2aa0fcc8d9c2c41e6c1b221d6b0af3ac
Bryan McCormick <bryan@mccormick.sh>
2026-03-10 17:51:33 -0600

    moved unbound to the host

--- host/server-setup.sh
+++ host/server-setup.sh
@@ -2,15 +2,15 @@
 
 apt update && \
     apt install \
-    apt-listchanges bsd-mailx htop ncdu rsync fish fail2ban lsof
+    htop ncdu rsync fish fail2ban lsof micro ufw unbound
 
-apt remove exim4-base exim4-config exim4-daemon-light &&
-    apt autoremove
+apt remove exim4 exim4-base exim4-config exim4-daemon-light &&
+    apt autoremove --purge
 
 groupadd -g 5000 vmail
 useradd -u 5000 -g vmail -s /usr/bin/false -M vmail
 
-useradd -s /usr/bin/fish -G docker,sudo,users bryan
+useradd -m -s /usr/bin/fish -G docker,sudo,users bryan
 
 TZ=America/Denver
 ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
